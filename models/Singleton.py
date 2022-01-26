8a36b73f236e1b4e3ca5b72acfc2cf4c36191096
Jose Alejandro Concepcion Alvarez <jalejandroc2928@gmail.com>
2022-01-26 14:18:33 +0100

    testing

--- models/Singleton.py
+++ models/Singleton.py
@@ -1,7 +1,25 @@
-class Singleton(object):
-    _instance = None
+# class Singleton(object):
+#     _instance = None
 
-    def __new__(class_, *args, **kwargs):
-        if not isinstance(class_._instance, class_):
-            class_._instance = object.__new__(class_, *args, **kwargs)
-        return class_._instance
+#     def __new__(class_, *args, **kwargs):
+#         print("CLASE DEL SINGLETON", class_)
+#         if not isinstance(class_._instance, class_):
+#             class_._instance = object.__new__(class_, *args, **kwargs)
+#         return class_._instance
+
+class Singleton:
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        """ Static access method. """
+        if Singleton.__instance == None:
+            Singleton()
+        return Singleton.__instance
+
+    def __init__(self):
+        """ Virtually private constructor. """
+        if Singleton.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            Singleton.__instance = self
