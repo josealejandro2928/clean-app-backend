80d86c2e849c10aad8f083f45c3b871606c7588b
Jose Alejandro Concepcion Alvarez <jalejandroc2928@gmail.com>
2022-01-26 23:21:42 +0100

    feat:testing

--- application.py
+++ application.py
@@ -1,9 +1,11 @@
 from main import application
 from models.ML_Model import ML_Model
 import os
+
+model = ML_Model()
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', default=3333))
-    model = ML_Model()
+    
     # from waitress import serve
     # import logging
     # logger = logging.getLogger('waitress')
