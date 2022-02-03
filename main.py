227f6c32e75ae0ac6770c4199f2068b3f17ada6d
Jose Alejandro Concepcion Alvarez <jalejandroc2928@gmail.com>
2022-02-03 10:40:10 +0100

    fix

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from flask_cors import CORS
 
 jinja_options = Flask.jinja_options
 jinja_options.update(dict(
-    block_start_string='$$',
-    block_end_string='$$',
-    variable_start_string='$',
-    variable_end_string='$',
-    comment_start_string='$#',
-    comment_end_string='#$',
+    block_start_string="%/",
+    block_end_string="\%",
+    variable_start_string="[%",
+    variable_end_string="%]",
+    comment_start_string='%$#',
+    comment_end_string='#$%',
 ))
 
 application = Flask(__name__, template_folder="static", static_folder="static")
